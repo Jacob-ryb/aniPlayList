27ba146446d10751f24f182fbb3a0bf791c640dc
Jacob-ryb <jacob.rybalsky@gmail.com>
2017-07-21 01:30:14 -0400

    Fixed URL routing and added a basic template

--- playlist/urls.py
+++ playlist/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
+
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'), #replace with homepage
-    url(r'^(?P<playlist_id>[0-9]+)/$', views.results, name='results'), #each users playlist , attempting to make it the full link was an awful idea
+    url(r'^(?P<playlist_id>[0-9]+)/$', views.result, name='results'), #each users playlist , attempting to make it the full link was an awful idea
 ]
--- playlist/views.py
+++ playlist/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import PlayList
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the playlist index.")
+    return HttpResponse("Please Enter a playlist ID")
 
-def results(request, playList_id):
-    response = 'You are looking at %s. playlist'
-    return response % playList_id
+
+def result(request, playlist_id):
+    playlist = get_object_or_404(PlayList, pk=playlist_id)
+    print(playlist.mal_or_kitsu_link)
+    return render(request, 'result.html', {'playlist': playlist})
